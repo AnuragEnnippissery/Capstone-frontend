3a4b66d5aa0754fe2e179a546e02c73998cf1473
AnuragEnnippissery <anuragramesh608@gmail.com>
2025-08-16 17:59:39 +0530

    navbar issue resolved with username added in navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,75 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import './Header.css'
 import { useNavigate } from "react-router-dom";
+import { AiFillHome } from "react-icons/ai";
+import { MdSubscriptions } from "react-icons/md";
+import { BiMoviePlay } from "react-icons/bi"; // using for Shorts
+import { FiMenu } from "react-icons/fi";
+import { useState ,useEffect} from "react";
 
 function Header() {
     let navigate=useNavigate()
+    const [collapsed, setCollapsed] = useState(false);
+    const [user, setUser] = useState("");
+
+    useEffect(() => {
+        const storedUser = sessionStorage.getItem("username");
+        if (storedUser) {
+            setUser(storedUser);
+        }
+    }, []);
+
   function HandleClick(){
     navigate('/Login')
   }
+
   return (
     <div>
         <div className="top-nav">
-            <h2>Youtube</h2>
-            <button onClick={HandleClick}>Sign In</button>
+          <h2>Youtube</h2>
+          <div className="user">
+               {user ? (
+                  <p> {user}!</p> // show username if logged in
+                ) : (
+                  <button onClick={HandleClick}>Sign In</button> // show sign in button only if no user
+                )}
+          </div>
+          
+         
         </div>
-        
-    <nav className="Navbar">
+
+    <div className="container">
+        <nav className={`Navbar ${collapsed ? "collapsed" : ""}`}>
+        <button className="hamburger" onClick={() => setCollapsed(!collapsed)}>
+          <FiMenu size={24} />
+        </button>
       <ul className="list">
         <li className="list-items">
-          <Link to="/">Home</Link>
+          <Link to="/">
+           <AiFillHome size={20} style={{ marginRight: "8px" }} />
+           {!collapsed && <span className="label">Home</span>}
+          </Link>
         </li>
         <li className="list-items">
-          <Link to="/">Shorts</Link>
+          <Link to="/">
+          <BiMoviePlay size={20} style={{ marginRight: "8px" }} />
+          {!collapsed && <span className="label">Shorts</span>}
+          </Link>
         </li>
         <li className="list-items">
-          <Link to="/">Subsciptions</Link>
+          <Link to="/">
+          <MdSubscriptions size={20} style={{ marginRight: "8px" }} />
+          {!collapsed && <span className="label">Subscription</span>}
+          </Link>
         </li>
       </ul>
     </nav>
+
+    <div className="content">
+      <Outlet/>
+    </div>
+    </div>   
+      
     </div>
     
   );
